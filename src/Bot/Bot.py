aebe9400c5c0cb1cc6330caca56c5348b211b384
Clement-Z4RM <clement.lienard@epitech.eu>
2024-11-14 00:13:04 +0100

    feat(bot): add a `reset_map` method to reset the map
    Also, the map can now be rect.

--- src/Bot/Bot.py
+++ src/Bot/Bot.py
@@ -16,12 +16,25 @@ class Bot:
         self.commands = Commands(self, logger)
         self.logger.info(self.information())
 
+    def reset_map(self, width=None, height=None):
+        if width is None:
+            if self.map is not None:
+                width = len(self.map[0])
+            else:
+                raise ValueError("Width is not provided and map is not initialized")
+        if height is None:
+            if self.map is not None:
+                height = len(self.map)
+            else:
+                height = width
+        self.map = [[Cell.Empty for _ in range(width)] for _ in range(height)]
+
     def play(self):
         self.logger.debug(f"{self.information.name} is playing")
-        x = randrange(len(self.map))
+        x = randrange(len(self.map[0]))
         y = randrange(len(self.map))
         while self.map[y][x] != Cell.Empty:
-            x = randrange(len(self.map))
+            x = randrange(len(self.map[0]))
             y = randrange(len(self.map))
         self.map[y][x] = self.player
         print(f"{x},{y}\r")

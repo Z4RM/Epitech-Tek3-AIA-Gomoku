0401fa2710edf958e254f969e59e00a8ce1e0be0
Clement-Z4RM <clement.lienard@epitech.eu>
2024-11-14 00:14:31 +0100

    feat(commands): implement command suites and implement/add "BOARD" command (suite)

--- src/Commands/Commands.py
+++ src/Commands/Commands.py
@@ -3,6 +3,7 @@ from src.Log.Logger import Logger
 from src.Enums.Player import Player
 from src.Enums.Cell import Cell
 from src.Commands.Command import Command
+from src.Commands.CommandSuite import CommandSuite
 
 START_REQUIREMENT = ["START", "RECTSTART"]
 
@@ -17,7 +18,7 @@ class Commands:
             "RECTSTART": Command(self.rectstart),
             "RESTART": Command(self.restart, [START_REQUIREMENT]),
             "SWAP2BOARD": Command(self.swap2board, [START_REQUIREMENT]),
-            "BOARD": Command(self.board, [START_REQUIREMENT]),
+            "BOARD": Command(CommandSuite(self.board_suite_iteration, "DONE", self.board_suite_end), [START_REQUIREMENT]),
             "BEGIN": Command(self.begin, [START_REQUIREMENT]),
             "INFO": Command(self.info),
             "TURN": Command(self.turn, [START_REQUIREMENT]),
@@ -25,19 +26,27 @@ class Commands:
             "TAKEBACK": Command(self.takeback, [START_REQUIREMENT]),
             "END": Command(self.end)
         }
+        self.suite = None
 
+    # region Command execution
     def execute(self, command):
-        command = command.split()
-        if len(command) < 1:
+        split_command = command.split()
+        command = split_command[0]
+        if len(split_command) < 1:
             self.logger.error("Received empty command")
             return
-        if command[0] not in self.commands:
-            self.unknown(f"Unknown command: {command[0]}")
+        if self.suite is not None:
+            return self.suite(split_command)
+        if command not in self.commands:
+            self.unknown(f"Unknown command: {command}")
             return
-        if not self.has_requirements(command[0]):
+        if not self.has_requirements(command):
             return
-        self.logger.debug(f"Executing command: {command[0]}")
-        return self.commands[command[0]](command)
+        self.logger.debug(f"Executing command: {command}")
+        if isinstance(self.commands[command].command, CommandSuite):
+            self.suite = self.commands[command].command
+            return self.suite(split_command)
+        return self.commands[command](split_command)
 
     def has_requirements(self, command):
         if self.commands[command].requirements is None:
@@ -55,6 +64,7 @@ class Commands:
                 print(f"{requirement} command must be executed before {command}\r")
                 return False
         return True
+    # endregion
 
     def about(self, _):
         print(self.bot.information())
@@ -65,7 +75,7 @@ class Commands:
         size = int(command[1])
         if size < 5:
             return self.error(f"Invalid size in START command: {size} (too small)")
-        self.bot.map = [[Cell.Empty for _ in range(size)] for _ in range(size)]
+        self.bot.reset_map(size)
         print("OK\r")
 
     def rectstart(self, _):
@@ -77,8 +87,26 @@ class Commands:
     def swap2board(self, _):
         return self.unknown("SWAP2BOARD command isn't yet implemented")
 
-    def board(self, _):
-        return self.unknown("BOARD command isn't yet implemented")
+    # region BOARD
+    def board_suite_init(self, command):
+        self.bot.reset_map()
+
+    def board_suite_iteration(self, command):
+        data = command[0].split(",")
+        if len(data) != 3:
+            return self.error(f"Invalid data: {command[0]}")
+        if data[2] != "1" and data[2] != "2":
+            return self.error(f"Invalid field: {data[2]}")
+        x = int(data[0])
+        y = int(data[1])
+        if x >= len(self.bot.map[0]) or y >= len(self.bot.map):
+            return self.error(f"Invalid coordinates: {x}, {y}")
+        self.bot.map[y][x] = Cell.from_int(int(data[2]))
+
+    def board_suite_end(self, _):
+        self.suite = None
+        self.bot.play()
+    # endregion
 
     def begin(self, _):
         self.bot.player = Player.Player1
@@ -110,6 +138,7 @@ class Commands:
     def end(_):
         return True
 
+    # region Errors
     def unknown(self, message):
         self.logger.error(message)
         print(f"UNKNOWN {message}\r")
@@ -119,3 +148,4 @@ class Commands:
         self.logger.error(message)
         print(f"ERROR {message}\r")
         return False
+    # endregion

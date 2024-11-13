22fa4884ea9595f1c94179d34229b052882f6df1
Clement-Z4RM <clement.lienard@epitech.eu>
2024-11-14 00:12:19 +0100

    feat(cell): add `from_int` static method to create a `Cell` from an integer

--- src/Enums/Cell.py
+++ src/Enums/Cell.py
@@ -6,3 +6,13 @@ class Cell(Enum):
     Empty = 0
     Player1 = Player.Player1
     Player2 = Player.Player2
+
+    @staticmethod
+    def from_int(value: int) -> "Cell":
+        if value == 0:
+            return Cell.Empty
+        if value == 1:
+            return Cell.Player1
+        if value == 2:
+            return Cell.Player2
+        raise ValueError(f"{value} is not a valid Cell")
